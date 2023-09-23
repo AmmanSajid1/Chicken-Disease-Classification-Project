33a4389fb67817db067189203ddddf0d6aa9bb8c
AmmanSajid1 <ammansajid1@gmail.com>
2023-09-23 23:15:54 +0100

    prepare callbacks added

--- src/Chicken_Disease_Classifaction_Project/config/configuration.py
+++ src/Chicken_Disease_Classifaction_Project/config/configuration.py
@@ -1,6 +1,7 @@
+import os
 from src.Chicken_Disease_Classifaction_Project.constants import *
 from src.Chicken_Disease_Classifaction_Project.utils.common import read_yaml, create_directories
-from src.Chicken_Disease_Classifaction_Project.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig)
+from src.Chicken_Disease_Classifaction_Project.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig)
 
 class ConfigurationManager:
     def __init__(self, config_filepath = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH):
@@ -41,3 +42,23 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+          config = self.config.prepare_callbacks
+          model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+          create_directories([
+                Path(model_ckpt_dir),
+                Path(config.tensorboard_root_log_dir)
+          ])
+
+          prepare_callback_config = PrepareCallbacksConfig(
+                root_dir=Path(config.root_dir),
+                tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+                checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+
+          )
+
+
+          return prepare_callback_config
