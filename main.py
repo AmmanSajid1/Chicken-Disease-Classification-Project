a6966fd5191689f0c0c01896d46a98dd82572a83
AmmanSajid1 <ammansajid1@gmail.com>
2023-09-24 00:31:03 +0100

    training added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.Chicken_Disease_Classifaction_Project.logger import logging
 from src.Chicken_Disease_Classifaction_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.Chicken_Disease_Classifaction_Project.exception import CustomException
 from src.Chicken_Disease_Classifaction_Project.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.Chicken_Disease_Classifaction_Project.pipeline.stage_03_training import ModelTrainingPipeline
 import sys
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -20,6 +21,18 @@ STAGE_NAME = "Prepare Base Model"
 try:
     logging.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logging.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x")
+        
+
+except Exception as e:
+    raise CustomException(e,sys)
+
+
+STAGE_NAME = "Training"
+try:
+    logging.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logging.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x")
         
--- src/Chicken_Disease_Classifaction_Project/config/configuration.py
+++ src/Chicken_Disease_Classifaction_Project/config/configuration.py
@@ -1,7 +1,7 @@
 import os
 from src.Chicken_Disease_Classifaction_Project.constants import *
 from src.Chicken_Disease_Classifaction_Project.utils.common import read_yaml, create_directories
-from src.Chicken_Disease_Classifaction_Project.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig)
+from src.Chicken_Disease_Classifaction_Project.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig)
 
 class ConfigurationManager:
     def __init__(self, config_filepath = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH):
@@ -62,3 +62,49 @@ class ConfigurationManager:
 
 
           return prepare_callback_config
+    
+
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+            config = self.config.prepare_callbacks
+            model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+            create_directories([
+                Path(model_ckpt_dir),
+                Path(config.tensorboard_root_log_dir)
+          ])
+
+            prepare_callback_config = PrepareCallbacksConfig(
+                root_dir=Path(config.root_dir),
+                tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+                checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+
+          )
+
+
+            return prepare_callback_config
+    
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+        create_directories([Path(training.root_dir)
+        
+        ])
+
+
+        training_config = TrainingConfig(
+              root_dir=Path(training.root_dir),
+              trained_model_path=Path(training.trained_model_path),
+              updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+              training_data=Path(training_data),
+              params_epoch=params.EPOCHS,
+              params_batch_size=params.BATCH_SIZE,
+              params_is_augmentation=params.AUGMENTATION,
+              params_image_size=params.IMAGE_SIZE
+
+        )
+
+        return training_config
